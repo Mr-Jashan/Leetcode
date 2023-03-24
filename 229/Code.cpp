f4bc9b489885ed46a70d0f6481e7c7852c055419
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-03-24 13:10:50 +0530

    Update Code.cpp

--- 229/Code.cpp
+++ 229/Code.cpp
@@ -1,1 +1,26 @@
-
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> ans;
+        unordered_map<int, int> map;
+        int n = nums.size(), j=0;
+        for(int i=0 ; i<n ; i++)
+        {
+            map[nums[i]]++;
+        }
+        for(auto [key, data]:map)
+        {
+            if(n%3==0) 
+            {
+                j = n/3 ;
+                if(data>j) ans.push_back(key);
+            }
+            else
+            {
+                j = (n/3)+1;
+                if(data>=j) ans.push_back(key);
+            }
+        }
+        return ans;
+    }
+};
