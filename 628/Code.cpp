93d1ee953a32ff8e0fa949c84c7f7ad222a209d1
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-03-24 15:46:46 +0530

    Update Code.cpp

--- 628/Code.cpp
+++ 628/Code.cpp
@@ -1,1 +1,11 @@
-
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n=nums.size(), ans=0, ans1, ans2;
+        ans1 = nums[n-1] * nums[n-2] * nums[n-3];
+        ans2 = nums[n-1] * nums[0] * nums[1];
+        ans = max(ans1, ans2);
+        return ans;
+    }
+};
