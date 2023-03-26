6f8c4011251f603a9d118d38efcfce0fabe16898
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-03-26 05:54:42 +0530

    Update Code.cpp

--- 747/Code.cpp
+++ 747/Code.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    int dominantIndex(vector<int>& nums) {
+        int b = INT_MIN, bi=-1, n=nums.size();
+        for(int i=0 ; i<n ; i++)
+        {
+            if(nums[i] > b)
+            {
+                b = nums[i];
+                bi = i;
+            }
+        }
+        sort(nums.begin(), nums.end());
+        if(nums[n-2]*2 <= b) return bi;
+        return -1;
+    }
+};
