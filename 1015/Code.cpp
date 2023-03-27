3188e4627e57fc75bb9d3687d98b922d3ca7ac2d
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-03-27 10:44:14 +0530

    Update Code.cpp

--- 1015/Code.cpp
+++ 1015/Code.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        unsigned long long sum=0;
+        for(int i=0 ; i<k ; i++)
+        {
+            sum = (sum*10 +1)%k;
+            if(sum ==0 ) return i+1;
+        }
+        return -1;
+    }
+};
