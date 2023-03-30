9fc1ba77a0b039a56c953865c3b049474fff5949
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-03-30 09:34:55 +0530

    Update Code.cpp

--- 9/Code.cpp
+++ 9/Code.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    bool isPalindrome(int x) 
+    {
+        string str = to_string(x); 
+        if(str[0] == '-')
+        {
+            return false;
+        }
+        else
+        {
+            for(int i=0, j=str.size()-1 ; i<str.size()-1, j>=0,i<=j ; i++, j--)
+            {
+                if(str[i] != str[j])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+};
