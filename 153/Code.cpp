d3db91175ad430a7cf03dbb4e27ab2aff7a124e4
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-03-31 13:34:17 +0530

    Update Code.cpp

--- 153/Code.cpp
+++ 153/Code.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l=0, r=nums.size()-1, min=0;
+        if(r+1 == 1) return nums[0];
+        while(l<r)
+        {
+            if(nums[l] < nums[r])
+            {
+                min = nums[l];
+                r--;
+            }
+            else
+            {
+                min = nums[r];
+                l++;
+            }
+        }
+        return min;
+    }
+};
