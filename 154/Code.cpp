f56ec6fd66aa172299d0a07709d51a68e464c8a7
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-01 10:47:04 +0530

    Update Code.cpp

--- 154/Code.cpp
+++ 154/Code.cpp
@@ -1,1 +1,27 @@
-
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l = 0, r = nums.size()-1, less=INT_MAX;
+        if(nums.size() == 1) return nums[0];
+        while(l<=r)
+        {
+            if(nums[l] > nums[r])
+            {
+                less = min(less, nums[r]);
+                l++;
+            }
+            else if(nums[l] < nums[r])
+            {
+                less = min(less, nums[l]);
+                r--;
+            }
+            else
+            {
+                less = min(less, nums[l]);
+                l++;
+                r--;
+            }
+        }
+        return less;
+    }
+};
