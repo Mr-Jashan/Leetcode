95af8ec7a5455e0f2b782a949ae21b7e3c593ada
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-03 14:35:55 +0530

    Update Code.cpp

--- 881/Code.cpp
+++ 881/Code.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n = people.size(), st = 0, et = n-1, count =0 ;
+        sort(people.begin(), people.end());
+        while(st <= et)
+        {
+            if(people[st] + people[et] <= limit)
+            {
+                count++;
+                st++;
+                et--;
+            }
+            else
+            {
+                count++;
+                et--;
+            }
+        }
+        return count;
+    }
+};
