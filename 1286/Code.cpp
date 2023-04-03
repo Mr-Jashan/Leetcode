d02545b5199dfa1d5942ce9a475ae38744a78bf6
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-03 15:06:44 +0530

    Update Code.cpp

--- 1286/Code.cpp
+++ 1286/Code.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+        int check = arr.size() / 4;
+        unordered_map <int,int> map;
+        for(int i=0 ; i<arr.size() ; i++)
+        {
+            map[arr[i]]++;
+        }
+        for(auto [key, data]: map)
+        {
+            if(data >= check+1) return key;
+        }
+        return -1;
+    }
+};
