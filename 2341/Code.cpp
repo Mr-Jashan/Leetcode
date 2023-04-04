a81f3e7ee4107b7b14ec04e3b8fce6ba7c15d9ee
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-04 15:05:41 +0530

    Update Code.cpp

--- 2341/Code.cpp
+++ 2341/Code.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    vector<int> numberOfPairs(vector<int>& nums) {
+        vector<int> ans;
+        int c1=0, c2=0, n = nums.size();
+        unordered_map<int, int> map;
+        for(int i=0 ; i<n ; i++) map[nums[i]]++;
+        for(auto [key, data]:map)
+        {
+            c1 += data / 2;
+            c2 += data % 2;
+        }
+        ans.push_back(c1);
+        ans.push_back(c2);
+        return ans;
+    }
+};
