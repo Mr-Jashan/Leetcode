946f1ef5fdc76710404d4915c6bd39829ffbc790
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-05 08:05:42 +0530

    Update Code.cpp

--- 2405/Code.cpp
+++ 2405/Code.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    int partitionString(string s) {
+        int count=1;
+        unordered_map<char, bool> map;
+        for(auto c:s)
+        {
+            if(map[c] == true) count++, map.clear();
+            map[c] =true;
+        }
+        return count;
+    }
+};
