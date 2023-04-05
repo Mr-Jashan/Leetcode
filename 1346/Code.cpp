f21963143823ceccf7967484595d27c0d6a09de9
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-05 12:35:06 +0530

    Update Code.cpp

--- 1346/Code.cpp
+++ 1346/Code.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        int n=arr.size();
+        for(int i=0 ; i<n ;  i++)
+        {
+            float temp = arr[i]/(float)2;
+            if(count(arr.begin(), arr.end(), 0) >1) return true;
+            if(count(arr.begin(), arr.end(), temp) != 0 && arr[i] !=0) return true;  
+        }
+        return false;
+    }
+};
