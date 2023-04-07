e2f5cd09491c84d89c2ccd240e039a1a805226f6
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-07 17:57:19 +0530

    Update Code.cpp

--- 2283/Code.cpp
+++ 2283/Code.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    bool digitCount(string num) {
+        map<int,int>map;
+        for(auto c:num)
+        {
+            map[c-'0']++;
+        }
+        for(int i=0 ; i<num.size() ; i++)
+        {
+            if(map[i] != num[i] - '0') return false;
+        }
+        return true;
+    }
+};
