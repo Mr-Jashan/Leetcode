408ab452292dbd8aafd3dd5f3e1d4f1fbe901e1e
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-07 18:08:18 +0530

    Update Code.cpp

--- 728/Code.cpp
+++ 728/Code.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    bool list(int n)
+    {
+        int temp = n;
+        while (temp!=0)
+        {
+            if(temp%10 ==0) return false;
+            else if(n%(temp%10) != 0) return false;
+            temp /= 10;
+        }
+        return true;
+    }
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> ans;
+        for(int i=left ; i<=right ; i++)
+        {
+            if(list(i)) ans.push_back(i);
+        }
+        return ans;
+    }
+};
