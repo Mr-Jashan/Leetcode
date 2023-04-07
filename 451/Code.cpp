cb4727338d7127da9c8072656d1899ddc474b04d
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-07 20:16:53 +0530

    Update Code.cpp

--- 451/Code.cpp
+++ 451/Code.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        string ans="";
+        unordered_map<char,int> map;
+        for(int i=0 ; i<s.size() ; i++) map[s[i]]++;
+        multimap<int, char> mmap;
+        for(auto &ab:map) mmap.insert({ab.second, ab.first});
+        for(auto [key, data]:mmap )
+        {
+            for(int i=1 ; i<=key ; i++) ans += data;
+        }
 
+        int l=0, r=ans.size()-1;
+        while(l<r)
+        {
+            swap(ans[l], ans[r]);
+            l++;
+            r--;
+        }
+        return ans;
+    }
+};
