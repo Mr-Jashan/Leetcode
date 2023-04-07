14b682eead870c17db731858085b2291b21da124
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-07 14:47:49 +0530

    Update Code.cpp

--- 2206/Code.cpp
+++ 2206/Code.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int, int> map;
+        for(int i=0 ; i<nums.size() ; i++)
+        {
+            map[nums[i]]++;
+        }   
+        for(auto [key, data]: map)
+        {
+            if(data%2 !=0) return false;
+        }
+        return true;
+    }
+};
