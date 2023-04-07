c89bb8e5ad091f6ce90173b0970084a7474ffefc
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-07 14:47:13 +0530

    Update Code.cpp

--- 1394/Code.cpp
+++ 1394/Code.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        unordered_map<int,int> map;
+        int ans = INT_MIN;
+    for(int i=0 ; i<arr.size() ; i++) map[arr[i]]++;
+        for(auto [key,data]:map)
+        {
+            if(key == data)
+            {
+                ans = max(ans, key);
+            }
+        }   
+        if(ans <0 ) return -1;
+        return ans;
+    }
+};
