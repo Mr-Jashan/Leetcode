8eb86b6bcbfda9f70c148091c1f9ed23e78eff40
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-07 16:14:42 +0530

    Update Code.cpp

--- 2441/Code.cpp
+++ 2441/Code.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int findMaxK(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size(), l=0, r=n-1;
+        while(l<r)
+        {
+            if(nums[0] >=0) return -1;
+            else if(nums[l]*(-1) == nums[r]) return nums[r];
+            else if(nums[l]*(-1) > nums[r]) l++;
+            else r--;
+        }
+        return -1;
+    }
+};
