ecd21fc837920624069f81802043733947142ac3
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-07 16:01:56 +0530

    Update Code.cpp

--- 2399/Code.cpp
+++ 2399/Code.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    bool checkDistances(string s, vector<int>& distance) {
+        int n = s.size();
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            for(int j=i+1 ; j<n ; j++)
+            {
+                if(s[i] == s[j])
+                {
+                    if((j-i-1) != distance[int(s[i]) - int('a')]) return false;
+                }
+            }
+        }
+        return true;
+    }
+};
