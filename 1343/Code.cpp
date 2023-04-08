11e5d2b119a9c884956549a3769aba910c4a1922
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-08 12:02:56 +0530

    Update Code.cpp

--- 1343/Code.cpp
+++ 1343/Code.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr, int k, int threshold) {
+        int sum=0;
+        for(int i=0 ; i<k ; i++)
+        {
+            sum += arr[i];
+        }
+        int count=0;
+        if(sum/k >= threshold) count++;
+        for(int i=k ; i<arr.size() ; i++)
+        {
+            sum += arr[i] - arr[i-k];
+            if(sum/k >= threshold) count++;
+        }
+        return count;
+    }
+};
