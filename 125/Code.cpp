2a153fc0c8f6f3434e02df8847993a5edac35e92
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-08 19:38:28 +0530

    Update Code.cpp

--- 125/Code.cpp
+++ 125/Code.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n=s.size();
+        string final="";
+        for(char c:s)
+        {
+            if(c>=48 && c<=57 || c>=97 &&c<=122) final +=c;
+            else if (c>=65 && c<=90) final += (c+32);
+            else continue;
+        } 
+        cout<<final;
+        int l = 0, r = final.size()-1;
+        while(l<r)
+        {
+            if(final[l] != final[r]) return false;
+            else l++, r--;
+        }
+        return true;
+    }
+};
