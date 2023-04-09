f59efbe52ff198c599476a13687ef483ec07ba16
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-09 12:19:41 +0530

    Update Code.cpp

--- 2068/Code.cpp
+++ 2068/Code.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    bool checkAlmostEquivalent(string word1, string word2) {
+        map<char, int> mp1;
+        map<char, int> mp2;
+        for(char c1:word1) mp1[c1]++;
+        for(char c2:word2) mp2[c2]++;
+        for(char ch:word1)
+        {
+            if(abs(mp1[ch] - mp2[ch]) >3 ) return false;
+        }
+        for(char ch:word2)
+        {
+            if(abs(mp1[ch] - mp2[ch]) >3 ) return false;
+        }
+        return true;
+    }
+};
