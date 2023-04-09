94688d46b2a57e97c6a61a644e845c26b4fcc6da
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-09 13:26:29 +0530

    Update Code.cpp

--- 1790/Code.cpp
+++ 1790/Code.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        int n = s1.size(), count=0;
+        string t1="", t2="";
+        for(int i=0 ; i<n ; i++)
+        {
+            if(s1[i] != s2[i]) 
+            {
+                t1 += s1[i];
+                t2 += s2[i];
+                count++;
+            }
+            if(count>2 )return false;
+        }
+        sort(t1.begin(), t1.end());
+        sort(t2.begin(), t2.end());
+        if(count ==1 || t1 != t2) return false; 
+        return true;
+    }
+};
