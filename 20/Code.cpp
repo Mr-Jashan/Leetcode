599d92af71e8b06a0ab86b25084b8fdba1aa1d33
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-10 08:10:18 +0530

    Update Code.cpp

--- 20/Code.cpp
+++ 20/Code.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.size() == 1) return false;
+        stack<char> sta;
+        for(char c:s)
+        {
+            if(sta.empty()) sta.push(c);
+            else if(c==')' && sta.top() == '(' || c==']' && sta.top() =='[' || c=='}' && sta.top() == '{' ) sta.pop();
+            else sta.push(c);
+        }
+        return sta.empty() ;
+    }
+};
