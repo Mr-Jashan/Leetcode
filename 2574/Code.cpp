9f7ca81f4e12b7dc26b46f53a099e4f7a0f5cb35
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-10 19:48:49 +0530

    Update Code.cpp

--- 2574/Code.cpp
+++ 2574/Code.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    vector<int> leftRigthDifference(vector<int>& nums) {
+       vector<int> ans;
+        int sum = 0, n = nums.size();
+        for(int x:nums) sum += x;
+        int lsum=0, rsum=sum;
+        for(int i=0 ; i<n ; i++)
+        {
+           rsum -= nums[i];
+           ans.push_back(abs(rsum - lsum));
+           lsum += nums[i];
+        }
+        return ans;
+    }
+};
