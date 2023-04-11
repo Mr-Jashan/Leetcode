8234f6d45ffd5cf2540a420435625c1af87245b7
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-11 13:05:16 +0530

    Update Code.cpp

--- 2085/Code.cpp
+++ 2085/Code.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int countWords(vector<string>& words1, vector<string>& words2) {
+        map<string, int> mp;
+        map<string, int> mp1;
+        int ans=0;
+        for(auto str:words1) mp[str]++;
+        for(auto str1:words2) mp1[str1]++;
+        for(auto str:words1)
+        {
+            if(mp[str] ==1 && mp1[str] ==1) ans++;
+        }
+        return ans;
+    }
+};
