e6275e60711c440cb2941dad49d9b4967399c418
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-11 12:08:16 +0530

    Update Code.cpp

--- 844/Code.cpp
+++ 844/Code.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        string s1="", t1="";
+        for(char c:s)
+        {
+            if(c=='#' && s1.empty()) continue; 
+            else if(c == '#') s1.pop_back();
+            else s1+=c;
+        }
+        for(char c:t)
+        {
+            if(c=='#' && t1.empty()) continue; 
+            else if(c == '#') t1.pop_back();
+            else t1+=c;
+        }
+        if(s1==t1)return true;
+        return false;
+    }
+};
