0d5a8ec38933bc1bcac7df9ae2d13840ebb18f79
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-11 09:16:14 +0530

    Update Code.cpp

--- 2390/Code.cpp
+++ 2390/Code.cpp
@@ -1,1 +1,26 @@
-
+class Solution {
+public:
+    string removeStars(string s) {
+        // int n=s.size();
+        stack<char> sta;
+        for(char c:s)
+        {
+            if(c != '*') sta.push(c);
+            else if(c == '*' && !sta.empty()) sta.pop();
+        }
+        string ans="";
+        while(!sta.empty())
+        {
+            ans += sta.top();
+            sta.pop();
+        }
+        int l=0, r=ans.size()-1;
+        while(l<r) 
+        {
+            swap(ans[l], ans[r]);
+            l++;
+            r--;
+        }
+        return ans;
+    }
+};
