0b6ff69a30e2fc848a8c83aaacbda0df2d5c8dd6
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-11 09:31:00 +0530

    Update Code.cpp

--- 1047/Code.cpp
+++ 1047/Code.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> sta;
+        int n = s.size();
+        for(int i=n-1 ; i>=0 ; i--)
+        {
+            if(sta.empty()) sta.push(s[i]);
+            else if(sta.top() == s[i]) sta.pop();
+            else sta.push(s[i]);
+        }
+        string ans="";
+        while(!sta.empty())
+        {
+            ans += sta.top();
+            sta.pop();
+        }
+        return ans;
+    }
+};
