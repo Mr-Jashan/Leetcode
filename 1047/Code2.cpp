dcef8ccb83f636a0a9e394fcbd6814fd366a89a8
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-11 09:34:44 +0530

    Update Code2.cpp

--- 1047/Code2.cpp
+++ 1047/Code2.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        int n = s.size();
+        string ans = "";
+        for(int i=0 ; i<n ; i++)
+        {
+            if(ans.back() == s[i]) ans.pop_back();
+            else ans.push_back(s[i]);
+        }
+        return ans;
+    }
+};
