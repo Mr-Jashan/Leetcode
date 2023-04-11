ce2a7561370fb902349296ebdbd1027ce9ab143e
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-11 09:19:48 +0530

    Update Code2.cpp

--- 2390/Code2.cpp
+++ 2390/Code2.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    string removeStars(string s) {
+        int n=s.size();
+        string ans="";
+        for(int i=0 ; i<n ; i++)
+        {
+            if(s[i] != '*') ans+= s[i];
+            else ans.pop_back(); 
+        }
+        return ans;
+    }
+};
