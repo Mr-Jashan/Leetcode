effb764a4310e488b505c558a59f040f4d46eab6
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-12 08:10:59 +0530

    Update Code2.cpp

--- 1598/Code2.cpp
+++ 1598/Code2.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        stack<char> sta;
+        int ans=0;
+        for(auto log:logs)
+        {
+            for(auto c:log)
+            {
+                if(c=='.') sta.push(c);
+                else if (c=='/') sta.push(c);
+            }
+            if(sta.size()==3 && ans!=0) ans--;
+            else if (sta.size()==1) ans++; 
+            while (!sta.empty())  sta.pop();
+        }
+        return ans;
+    }
+};
