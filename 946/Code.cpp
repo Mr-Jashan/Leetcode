18c43df7e7dd83edb5a52ae61a4a6707b3e1ae4f
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-13 19:30:34 +0530

    Update Code.cpp

--- 946/Code.cpp
+++ 946/Code.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int n=popped.size();
+        stack<int> sta;
+        stack<int> sta2; 
+        for(int i=n-1 ; i>=0 ; i--) sta2.push(popped[i]);
+        for(int x2:pushed)
+        {
+            sta.push(x2);
+            while(!sta.empty() && !sta2.empty() && sta.top() == sta2.top()) 
+            {
+                sta.pop();
+                sta2.pop();
+            }
+        }
+        if(sta.empty() && sta2.empty())return true;
+        return false;
+    }
+};
