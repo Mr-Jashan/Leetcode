8b29285e44ebd72d2a57e4c8f88ceb6a9ae1c180
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-13 15:56:25 +0530

    Update Code.cpp

--- 2465/Code.cpp
+++ 2465/Code.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int distinctAverages(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        unordered_map<float,int> mp;
+        int n = nums.size();
+        int l=0 , r=n-1;
+        while(l<r)
+        {
+            float temp =0;
+            temp = (nums[l] + nums[r])/(float)2;
+            mp[temp]++;
+            l++;
+            r--;
+        }
+        return mp.size();
+    }
+};
