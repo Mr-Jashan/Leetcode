53a0af50c32fee2d0fd6097d1c5b7894628c161e
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-17 08:40:23 +0530

    Update Code.cpp

--- 1431/Code.cpp
+++ 1431/Code.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) 
+    {
+        vector<bool> ans;
+        long int max  =*max_element(candies.begin(), candies.end());
+        for(int i=0 ; i<candies.size() ; i++)
+        {
+                if(candies[i]+extraCandies <max)
+                {
+                    ans.push_back(0);
+                } 
+                else
+                {
+                    ans.push_back(true);
+                }
+        }
+        return ans;
+    }
+};
