b672e5df5458ad7513725135f6c4010b855b0a1a
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-18 15:13:34 +0530

    Update Code.cpp

--- 1768/Code.cpp
+++ 1768/Code.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int st1=0, st2=0, n1=word1.size(), n2=word2.size();
+        string ans="";
+        while(st1 <= n1-1 || st2 <= n2-1)
+        {
+            if(st1 <= n1-1) 
+            {
+                ans += word1[st1];
+                st1++;
+            }
+            if(st2 <= n2-1) 
+            {
+                ans += word2[st2];
+                st2++;
+            }
+        }
+        return ans;
+    }
+};
