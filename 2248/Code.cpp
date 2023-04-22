81bfdbd5cd0a9171388c81f2877accb8565bb34e
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-22 19:59:32 +0530

    Update Code.cpp

--- 2248/Code.cpp
+++ 2248/Code.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    vector<int> intersection(vector<vector<int>>& nums) {
+        unordered_map<int,int> mp;
+        vector<int>ans;
+        for(auto vec:nums)
+        {
+            for(int x:vec) mp[x]++;
+        }
+        for(auto c:mp)  if(c.second == nums.size()) ans.push_back(c.first);
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
