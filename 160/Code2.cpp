86a22a395080ff6ff3f1e9e22a8d23e0212272c7
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-24 19:06:20 +0530

    Update Code2.cpp

--- 160/Code2.cpp
+++ 160/Code2.cpp
@@ -1,1 +1,27 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        unordered_map<ListNode*, int> mp;
+        ListNode*temp = headA;
+        while(temp!=NULL)
+        {
+            mp[temp]++;
+            temp = temp->next;
+        }
+        ListNode*temp2=headB;
+        while(temp2!=NULL)
+        {
+            if(mp[temp2] != NULL) return temp2;
+            temp2 = temp2->next;
+        }
+        return NULL;
+    }
+};
