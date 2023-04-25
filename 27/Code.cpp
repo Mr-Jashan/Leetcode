aa62ccf85e74ab1558bc9cee5748287cd10ed9b0
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-25 15:33:16 +0530

    Update Code.cpp

--- 27/Code.cpp
+++ 27/Code.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int ans=nums.size();
+        vector<int> temp;
+        for(int x:nums) temp.push_back(x);
+        nums.clear();
+        for(int x:temp) if(x==val) ans--;
+        for(int x:temp) if(x != val)nums.push_back(x);
+        return ans;
+    }
+};
