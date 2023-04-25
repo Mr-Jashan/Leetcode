ae89c2b18e0821cd2f8376dff48c02ff83be19a2
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-25 15:44:19 +0530

    Update Code.cpp

--- 26/Code.cpp
+++ 26/Code.cpp
@@ -1,1 +1,10 @@
-
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set<int> st;
+        for(int x:nums) st.insert(x);
+        nums.clear();
+        for(int x:st) nums.push_back(x);
+        return st.size();
+    }
+};
