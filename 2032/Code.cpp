452b0a56a236d60914e6158f192b936d84fda3c1
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-27 11:18:05 +0530

    Update Code.cpp

--- 2032/Code.cpp
+++ 2032/Code.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
+        vector<int>ans;
+        set<int>v1,v2,v3;
+        unordered_map<int,int> mp;
+        for(int x:nums1) v1.insert(x);
+        for(int x:nums2) v2.insert(x);
+        for(int x:nums3) v3.insert(x);
+        for(int x:v1) mp[x]++;
+        for(int x:v2) mp[x]++;
+        for(int x:v3) mp[x]++;
+        for(auto x:mp) 
+        {
+            if(x.second >1) ans.push_back(x.first);
+        }
+        return ans;
+    }
+};
