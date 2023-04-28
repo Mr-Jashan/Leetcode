854210f9a0cff5f086b830ee86ff87949d5856bd
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-28 18:51:14 +0530

    Update Code.cpp

--- 2000/Code.cpp
+++ 2000/Code.cpp
@@ -1,1 +1,10 @@
-
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int r = find(word.begin(), word.end(), ch) - word.begin();
+        int l=0;
+        if(r>word.size()-1) return word;
+        while(l<r) swap(word[l],word[r]), l++,r--;
+        return word;
+    }
+};
