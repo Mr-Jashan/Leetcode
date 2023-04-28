46b0650d92e809cd15e7556d9e7fee6af70ac32e
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-28 19:14:22 +0530

    Update Code.cpp

--- 557/Code.cpp
+++ 557/Code.cpp
@@ -1,1 +1,30 @@
-
+class Solution {
+public:
+    string reverseWords(string s) {
+        int l=0, r=0;
+        for(int i=0 ; i<s.size() ; i++)
+        {
+            if(s[i] !=' ') r++;
+            else
+            {
+                r--;
+                while(l<r) 
+                {
+                    swap(s[l],s[r]);
+                    l++;
+                    r--;
+                }
+                l=i+1;
+                r=i+1;
+            }
+        }
+        r--;
+        while(l<r) 
+        {
+            swap(s[l],s[r]);
+            l++;
+            r--;
+        }
+        return s;
+    }
+};
