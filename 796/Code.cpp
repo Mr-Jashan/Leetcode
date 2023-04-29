87d1a40dfff692bb0ee7a848fea691bb4340a924
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-29 16:31:11 +0530

    Update Code.cpp

--- 796/Code.cpp
+++ 796/Code.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n=0;
+        while(n<s.size())
+        {
+            char temp = s[0];
+            s.erase(0,1);
+            s += temp;
+            if(s == goal) return true;
+            else n++;
+        }
+        return false;
+    }
+};
