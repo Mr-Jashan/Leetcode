193f4d1e46a91e99df568ffe89e1a5c339d6e924
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-29 14:18:29 +0530

    Update Code.cpp

--- 1455/Code.cpp
+++ 1455/Code.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+        vector<string> temp;
+        string s;
+        for(char c:sentence) 
+        {
+            if(c!=' ') s+=c;
+            else 
+            {
+                temp.push_back(s);
+                s.clear();
+            }
+        }
+        temp.push_back(s);
+        for(int i=0 ; i<temp.size() ; i++)
+        {
+            string ss = temp[i].substr(0,searchWord.size());
+            if(ss == searchWord) return i+1;
+        }
+        return -1;
+    }
+};
