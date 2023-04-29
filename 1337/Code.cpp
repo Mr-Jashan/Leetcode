76185440d4d995eede32ab8ecbd82d30d4a12e2f
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-04-29 14:53:05 +0530

    Update Code.cpp

--- 1337/Code.cpp
+++ 1337/Code.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        map<int,int>mp;
+        vector<int> ans;
+        int z=0;
+        for(auto vec:mat)
+        {
+            mp[z] = count(vec.begin(), vec.end(), 1);
+            z++;
+        }
+        multimap <int,int> mmp;
+        for(auto x:mp) mmp.insert({x.second, x.first});
+        z=0;
+        for(auto x:mmp)
+        {
+            if(z == k) break;
+            ans.push_back(x.second);
+            z++;
+        }
+        return ans;
+    }
+};
