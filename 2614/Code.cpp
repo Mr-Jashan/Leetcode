572308b2d443b60990891edc9c540353c0cf0ea5
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-05-02 07:21:22 +0530

    Update Code.cpp

--- 2614/Code.cpp
+++ 2614/Code.cpp
@@ -1,1 +1,28 @@
-
+class Solution {
+public:
+    int diagonalPrime(vector<vector<int>>& nums) {
+        int ans=INT_MIN, n=nums.size();
+        vector<int> temp;
+        for(int i=0 ; i<n ; i++) temp.push_back(nums[i][i]);
+        for(int i=0 ; i<n ; i++) temp.push_back(nums[i][n-1-i]);
+        sort(temp.rbegin(), temp.rend());
+        int flag=0;
+        if(temp[0]==1)return 0;
+        if(temp[0]==2 || temp[0]==3) return temp[0];
+        for(int x:temp) 
+        {
+            flag=0;
+            if(x%2==0 || x%3==0) continue;
+            for(int i=5 ; i<x ; i+=2) 
+            {
+                if(x%i ==0)
+                {
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0) return x; 
+        }
+        return 0;
+    }
+};
