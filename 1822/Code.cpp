321c4a5149cd4852681a5057a38b20da289d0164
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-05-02 06:28:14 +0530

    Update Code.cpp

--- 1822/Code.cpp
+++ 1822/Code.cpp
@@ -1,1 +1,10 @@
-
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        if(count(nums.begin(), nums.end(), 0) != 0) return 0;
+        int temp=0;
+        for(int x:nums) if(x<0) temp++;
+        if(temp%2==0) return 1;
+        else return -1;
+    }
+};
