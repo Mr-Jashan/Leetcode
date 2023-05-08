4a78359f7ee310686316f7652108437b5028dfa9
Jashan <72308753+Mr-Jashan@users.noreply.github.com>
2023-05-08 16:32:24 +0530

    Update Code.cpp

--- 1572/Code.cpp
+++ 1572/Code.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int ans=0, n = mat.size(), i=0;
+        for(i=0 ; i<n ; i++)
+        {
+            ans += mat[i][i];
+        }
+        i = n-1;
+        for(int j=0 ; j<n ; j++)
+        {
+            ans += mat[i][j];
+            i--;
+        }
+        i = n/2;
+        if(n%2!=0) ans -= mat[i][i];
+        return ans;
+    }
+};
